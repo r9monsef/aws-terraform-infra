55b99a9431fbf747d26fa54eba689660347316fa
Reza Shams <reza.shamsimonsef@gmail.com>
2025-05-09 17:02:55 +0000

    deleted all

--- environments/prod/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region where resources will be created"
-  default     = "us-east-1"
-}
-
-variable "prod_vpc_cidr" {
-  description = "CIDR block for the production VPC"
-  default     = "10.0.0.0/16"
-}
--- environments/stage/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region      = "us-east-1"
-stage_vpc_cidr  = "10.1.0.0/16"
